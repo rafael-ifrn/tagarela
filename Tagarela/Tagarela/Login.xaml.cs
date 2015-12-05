2ba1c49950b8e5f8b6963b107efa4ccd7f02a3b7
rafael <rafael.ifrn.2011@gmail.com>
2015-12-05 14:00:37 -0300

    Commit inicial do projeto
    Iniciado tela de login e recuperação de uri de notificação

---
+++ Tagarela/Tagarela/Login.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Notification;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Tagarela
+{
+    public partial class Login : PhoneApplicationPage
+    {
+
+        private string channelName = "tagarelaIFRN";
+        private string servidor = "https://meumsn.herokuapp.com";
+        private string uri = "";
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEntrar_Click(object sender, RoutedEventArgs e)
+        {
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(servidor);
+
+            canalNotificacao();
+            canalNotificacao();
+
+            Model.Autenticacao a = new Model.Autenticacao
+            {
+                username = txtLogin.Text,
+                senha = txtSenha.Text,
+                uri = uri
+            };
+
+            MessageBox.Show(String.Format("O canal URI é {0}", uri));
+
+            string s = "=" + JsonConvert.SerializeObject(a);
+
+            var content = new StringContent(s, Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            var response = await httpClient.PostAsync("/api/user/login", content);
+
+            var str = response.Content.ReadAsStringAsync().Result;
+
+            str = "OK";
+
+            if (str == "OK")
+            {
+                NavigationService.Navigate(new Uri("/login.xaml", UriKind.Relative));
+            }
+        }
+
+
+
+        private void canalNotificacao()
+        {
+            // Recupera o canal se existir
+            HttpNotificationChannel httpChannel = HttpNotificationChannel.Find(channelName);
+
+            // Verifica se o canal existe
+            if (httpChannel == null)
+            {
+                // Canal não existe. Instancia novo canal
+                httpChannel = new HttpNotificationChannel(channelName);
+
+                // Delegates para atualização, erro e recebimento de mensagem
+                httpChannel.ChannelUriUpdated += new EventHandler<NotificationChannelUriEventArgs>(httpChannel_ChannelUriUpdated);
+                httpChannel.ErrorOccurred += new EventHandler<NotificationChannelErrorEventArgs>(httpChannel_ErrorOccurred);
+                httpChannel.ShellToastNotificationReceived += new EventHandler<NotificationEventArgs>(httpChannel_ShellToastNotificationReceived);
+
+                // Abre o canal e efetiva a ligação dos recebimentos
+                httpChannel.Open();
+                httpChannel.BindToShellToast();
+                
+            }
+            else
+            {
+                // Canal existe
+                // Delegates para atualização, erro e recebimento de mensagem
+                httpChannel.ChannelUriUpdated += new EventHandler<NotificationChannelUriEventArgs>(httpChannel_ChannelUriUpdated);
+                httpChannel.ErrorOccurred += new EventHandler<NotificationChannelErrorEventArgs>(httpChannel_ErrorOccurred);
+                httpChannel.ShellToastNotificationReceived += new EventHandler<NotificationEventArgs>(httpChannel_ShellToastNotificationReceived);
+                // Mostra dados do canal
+                System.Diagnostics.Debug.WriteLine(httpChannel.ChannelUri.ToString());
+                //MessageBox.Show(String.Format("O canal URI é {0}", httpChannel.ChannelUri.ToString()));
+                uri = httpChannel.ChannelUri.ToString();
+                // Atualiza serviço de usuários
+                //atualizarUsuario(txtUsuario.Text, httpChannel.ChannelUri.ToString());
+            }
+        }
+
+        private void httpChannel_ChannelUriUpdated(object sender, NotificationChannelUriEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                uri = e.ChannelUri.ToString();
+                // Mostra dados do canal
+                System.Diagnostics.Debug.WriteLine(e.ChannelUri.ToString());
+                // MessageBox.Show(String.Format("O canal URI é {0}", e.ChannelUri.ToString()));
+                // Atualiza serviço de usuários
+                //atualizarUsuario(txtUsuario.Text, e.ChannelUri.ToString());
+            });
+        }
+
+        private void httpChannel_ErrorOccurred(object sender, NotificationChannelErrorEventArgs e)
+        {
+            // Mostra dados do erro
+            Dispatcher.BeginInvoke(() => MessageBox.Show(String.Format("A push notification {0} error occurred.  {1} ({2}) {3}",
+                e.ErrorType, e.Message, e.ErrorCode, e.ErrorAdditionalData)));
+        }
+
+        private void httpChannel_ShellToastNotificationReceived(object sender, NotificationEventArgs e)
+        {
+            StringBuilder message = new StringBuilder();
+            string relativeUri = string.Empty;
+            message.AppendFormat("Toast Recebido {0}:\n", DateTime.Now.ToShortTimeString());
+
+            // Parse out the information that was part of the message.
+            foreach (string key in e.Collection.Keys)
+                message.AppendFormat("{0}: {1}\n", key, e.Collection[key]);
+
+            // Display a dialog of all the fields in the toast.
+            Dispatcher.BeginInvoke(() =>
+            {
+                MessageBox.Show(message.ToString());
+                /*listMsg.Items.Add(
+                    e.Collection["wp:Text1"] + ": " + e.Collection["wp:Text2"]); */
+            });
+        }
+    }
+}
---
+++ Tagarela/Tagarela/Model/Autenticacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tagarela.Model
+{
+    class Autenticacao
+    {
+        public string username { get; set; }
+        public string senha { get; set; }
+        public string uri { get; set; }
+    }
+}
